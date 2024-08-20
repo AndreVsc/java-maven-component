fdfd8096b53c0a545d36fdcda5e6a842f789657c
AndreVsc <95365578+AndreVsc@users.noreply.github.com>
2024-08-20 19:43:20 -0300

    done

--- src/main/java/andrevsc/App.java
+++ src/main/java/andrevsc/App.java
@@ -1,7 +1,13 @@
 package andrevsc;
 
 public class App 
-{
+{   
+    /*
+      USO:
+      <groupId>junit</groupId>
+      <artifactId>junit</artifactId>
+      <version>4.11</version>
+    */
     public static void main( String[] args )
     {
         System.out.println( "Componente feito por Andre" );
