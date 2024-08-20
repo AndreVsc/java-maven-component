738770e7caa765f9275f75cb4fa9274204c2cc55
AndreVsc <95365578+AndreVsc@users.noreply.github.com>
2024-08-20 19:29:36 -0300

    done

--- src/main/java/andrevsc/App.java
+++ src/main/java/andrevsc/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Componente feito por Andre" );
     }
 }
